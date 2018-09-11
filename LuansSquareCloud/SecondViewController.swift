eccb43af12e78d92dace10fa8fcf66e903d0be9e
cho wanhee <chowanhee@choui-macbook-air.local>
2018-09-11 16:04:31 +0900

    Initial Commit

---
+++ LuansSquareCloud/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LuansSquareCloud
+//
+//  Created by cho wanhee on 2018. 9. 11..
+//  Copyright © 2018년 cho wanhee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
